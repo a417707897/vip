345bdbf6f80d82a5106e9b2ef3cc6d5ff1a074c6
zyl <1273641404@qq.com>
2019-12-27 20:40:27 +0800

    影视表的增加

--- src/main/java/cn/luckylin/vip/bean/MovieDetails.java
+++ src/main/java/cn/luckylin/vip/bean/MovieDetails.java
@@ -2,6 +2,8 @@ package cn.luckylin.vip.bean;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Table;
 import java.util.Date;
 
 /**
@@ -10,47 +12,87 @@ import java.util.Date;
  * @Date: 2019/12/27
  */
 @Data
+@Table(name = "MOVIE_DETAILS")
 public class MovieDetails {
 
     //自增长id
+    @Column(name = "ID")
     private Integer id;
+
     //影视id，也是影视唯一标识
-    private Integer movieId;
+    @Column(name = "MOVIE_ID")
+    private String movieId;
+
     //影视标题
+    @Column(name = "TITLE")
     private String title;
+
     //影视导演
+    @Column(name = "DIRECTOR")
     private String director;
+
     //影视演员
+    @Column(name = "ACTOR")
     private String actor;
+
     //封面图片链接
+    @Column(name = "IMAGE_URL")
     private String imageUrl;
+
     //影视评分
+    @Column(name = "SCORE")
     private String score;
+
     //影视类型
+    @Column(name = "TYPE")
     private String type;
+
     //影视地区
+    @Column(name = "AREA")
     private String area;
+
     //影视语言
+    @Column(name = "LANGUAGE")
     private String language;
+
     //上映时间
+    @Column(name = "RELEASE_TIME")
     private String releaseTime;
+
     //影视片长
+    @Column(name = "MOVIE_LENGTH")
     private String movieLength;
+
     //影视更新时间
+    @Column(name = "MOVIE_UPDATE_TIME")
     private String movieUpdateTime;
+
     //影视介绍
+    @Column(name = "INTRODUCTION")
     private String introduction;
+
     //影视总播放量
+    @Column(name = "TOTAL_PLAYBACK")
     private String totalPlayback;
+
     //今日播放量
+    @Column(name = "TODAY_PLAYBACK")
     private String todayPlayback;
+
     //创建时间
+    @Column(name = "CREATE_DATE")
     private Date createDate;
+
     //创建人
+    @Column(name = "CREATE_BY")
     private String createBy;
+
     //更新时间
+    @Column(name = "UPDATE_DATE")
     private Date updateDate;
+
     //更新人
+    @Column(name = "UPDATE_BY")
     private String updateBy;
 
 }
--- src/main/java/cn/luckylin/vip/bean/MovieUrl.java
+++ src/main/java/cn/luckylin/vip/bean/MovieUrl.java
@@ -1,7 +1,8 @@
 package cn.luckylin.vip.bean;
 
 import lombok.Data;
-
+import javax.persistence.Column;
+import javax.persistence.Table;
 import java.util.Date;
 
 /**
@@ -10,20 +11,34 @@ import java.util.Date;
 * @Date: 2019/12/27
 */
 @Data
+@Table(name = "MOVIE_URL")
 public class MovieUrl {
 
     //自增长id
+    @Column(name = "ID")
     private Integer id;
+
     //影视id
-    private Integer movieId;
+    @Column(name = "MOVIE_ID")
+    private String movieId;
+
     //影视集数
+    @Column(name = "SET_NUM")
     private String setNum;
+
     //影视集数对应的url
+    @Column(name = "URL")
     private String url;
+
     //标记，这个什么类型的下载链接
+    @Column(name = "FLAG")
     private String flag;
+
     //创建时间
+    @Column(name = "CREATE_DATE")
     private Date createDate;
+
     //创建人
+    @Column(name = "CREATE_BY")
     private String createBy;
 }
